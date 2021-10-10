98313cffa543fc215b180fda37d9ba27a12bb258
Saul Luna Minor <40225160+tiocalvispolimarch@users.noreply.github.com>
2021-10-10 16:40:11 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { join } = require('path');
+const path = require('path');
+
+const app = express();
+
+
+
+app.get('/',(req, res)=>  {
+res.sendFile(path.join(__dirname)+'/Views/index.html');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, _ =>{
+
+    console.log( `Aplicacion desarrollada en el puerto ${PORT}` );
+});
+
+
