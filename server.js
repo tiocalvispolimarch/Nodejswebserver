7276d9b30ebf2dd97d2ba86dddb9c68145e77531
Patiño <Patio@DESKTOP-NM1U7H0>
2021-10-11 17:32:52 -0500

    r

--- server.js
+++ server.js
@@ -5,4 +5,15 @@ document.body.innerHTML = "<h2>Time right now is:  " + d.getHours() + ":" + d.ge
 "</h2>";
 
 
+
+const server = http.createServer((req, res) => {
+    // we can access HTTP headers
+    req.on('data', (chunk) => {
+      console.log(`Data chunk available: ${chunk}`)
+    })
+    req.on('end', () => {
+      //end of data
+    })
+  })
+  
 console.log("Hello world!");
