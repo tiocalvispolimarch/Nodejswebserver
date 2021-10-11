8a62b913946da6543a402b6b83bc95299c43674e
Patiño <Patio@DESKTOP-NM1U7H0>
2021-10-11 13:57:44 -0500

    r

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.listen(PORT, _ =>{
 });
 
 let d = new Date();
-document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h1>"
+document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h1>
 
