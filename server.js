10a79283a9c85e6e902e2037da9d3655ee7dd64f
Patiño <Patio@DESKTOP-NM1U7H0>
2021-10-11 14:05:43 -0500

    r

--- server.js
+++ server.js
@@ -1,20 +1,4 @@
-const express = require('express');
-const { join } = require('path');
-const path = require('path');
 
-const app = express();
-
-
-
-app.get('/',(req, res)=>  {
-res.sendFile(path.join(__dirname)+'/Views/index.html');
-});
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, _ =>{
-
-    console.log( `Aplicacion desarrollada en el puerto ${PORT}` );
-});
 
 let d = new Date();
 document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
