057ddeb3f8de1b31149a090523090afcf65422e6
Patiño <Patio@DESKTOP-NM1U7H0>
2021-10-11 14:10:11 -0500

    g

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 
 let d = new Date();
-document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-"</h1>";
+document.body.innerHTML = "<h2>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+"</h2>";
 
 
