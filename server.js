e2395e992d7d39c11659a5914c450c79f8828593
Patiño <Patio@DESKTOP-NM1U7H0>
2021-10-15 19:33:17 -0500

    h

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ let d = new Date();
 document.body.innerHTML = "<h2>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
 "</h2>";
 
+const data = firebase.database();
+//Obtener una referencia a la raíz de la base de datos
+let refToData = data.ref()
+//Obtener una console.log de todos los datos 
+dataRef.once('value', snapshot => {
+  console.log(snapshot.val());
+});
 
 
 console.log("Hello world!");
