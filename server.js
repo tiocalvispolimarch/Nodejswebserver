dd85da6d911d9f1660eb0cf48d24d8bb80c41cb8
Patiño <Patio@DESKTOP-NM1U7H0>
2021-10-15 19:47:08 -0500

    w

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ document.body.innerHTML = "<h2>Time right now is:  " + d.getHours() + ":" + d.ge
 
 
       // Import the functions you need from the SDKs you need
-     // import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-      //import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
+      import  initializeApp  from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+      import  getAnalytics  from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
 
-      import {  } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-      import {  } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
+      //import {  } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+      //import {  } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
       // TODO: Add SDKs for Firebase products that you want to use
       // https://firebase.google.com/docs/web/setup#available-libraries
     
